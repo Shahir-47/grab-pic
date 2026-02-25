fa17664a15d933990a30617978479a57caf88357
Shahir Ahmed <ahmeds@dickinson.edu>
2026-02-24 21:20:52 -0500

    Add JsonProperty annotation for isPublic field in PhotoResponse

--- api/src/main/java/com/grabpic/api/dto/PhotoResponse.java
+++ api/src/main/java/com/grabpic/api/dto/PhotoResponse.java
@@ -2,6 +2,7 @@ package com.grabpic.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
@@ -10,7 +11,10 @@ import java.util.List;
 public class PhotoResponse {
     private String id;
     private String viewUrl;
+
+    @JsonProperty("isPublic")
     private boolean isPublic;
+
     private boolean processed;
     private int faceCount;
     private List<String> faceBoxes;
